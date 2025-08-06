bd05af212dab5c640fac3f378985c8a56d7f1988
Rios-Jaime <Hiemlick@gmail.com>
2025-08-06 11:46:40 -0700

    removed redundant files

--- run_validator.py
+++
@@ -1,5 +0,0 @@
-from utils.validator_class import validate_project
-
-if __name__ == "__main__":
-    project_name = input("Enter the project path to validate: ").strip()
-    validate_project(project_name)
